53fd1e8ca0b9a69b756f9a7711d103c6e196f204
jfishB <jeeveshbalendra@gmail.com>
2024-07-13 19:39:04 -0400

    start

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,50 @@
+import styles from "./HeroStyles.module.css";
+import heroImg from "../../assets/hero-img.png";
+import themeIcon from "../../assets/sun.svg";
+import twitterIcon from "../../assets/twitter-light.svg";
+import githubIcon from "../../assets/github-light.svg";
+import linkedinIcon from "../../assets/linkedin-light.svg";
+import CV from "../../assets/cv.pdf";
+import { useTheme } from "../../common/ThemeContext";
+
+function Hero() {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <section id="hero" className={styles.container}>
+      <div className={styles.colorModeContainer}>
+        <img className={styles.hero} src={heroImg} alt="Profile picture" />
+        <img
+          className={styles.colorMode}
+          src={themeIcon}
+          alt="Color mode icon"
+        />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          Jeevesh
+          <br />
+          Balendra
+        </h1>
+        <h2>Software Developer</h2>
+        <span>
+          <a href="https://twitter.com/" target="_blank">
+            <img src={twitterIcon} alt="Twitter icon" />
+          </a>
+          <a href="https://github.com/" target="_blank">
+            <img src={githubIcon} alt="Github icon" />
+          </a>
+          <a href="https://linkedin.com/" target="_blank">
+            <img src={linkedinIcon} alt="LinkedIn icon" />
+          </a>
+        </span>
+        <p>With a passion for all kinds of problem solving!</p>
+        <a href={CV} download>
+          <button className="hover">Resume</button>
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
