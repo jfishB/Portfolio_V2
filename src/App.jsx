be7acd9cfc994382f7a556e6d8f55206d9f5533d
Jeevesh Balendra <jeeveshbalendra@gmail.com>
2025-08-14 01:31:35 -0400

    begin new design

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
 import "./App.css";
-import Hero from "./sections/Hero/Hero";
-import Projects from "./sections/Projects/Projects";
-import Skills from "./sections/Skills/Skills";
-import Experience from "./sections/Experience/Experience";
-import Footer from "./sections/Footer/Footer";
 
 function App() {
   return (
-    <>
-      <Hero />
-      <Projects/>
-      <Experience/>
-      <Skills/>
-      <Footer/>
-    </>
+    <div>
+      <div className="header-row">
+        {/* name */}
+        <h1>Jeevesh Balendra</h1>
+        {/* socials */}
+        <div className="socials">
+          <a
+            href="https://github.com/jfishB"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          <a
+            href="https://www.linkedin.com/in/jeevesh0126"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </div>
+      </div>
+      {/* description */}
+      <p className="description">
+        Software Engineer based in Toronto, building in the intersection of
+        Healthcare and AI. Studying Computer Science at the University of
+        Toronto.
+        <br />
+        <br />
+        Currently building at SickKids Hospital.
+      </p>
+      {/* projects */}
+      <div className="projects">{/* <h2>Projects</h2> */}</div>
+      {/* experience */}
+      <div className="experience">{/* <h2>Experience</h2> */}</div>
+    </div>
   );
 }
 
